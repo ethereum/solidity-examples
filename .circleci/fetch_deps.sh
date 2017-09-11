4d3f07a3d0c4577786232ce04eef32264dd52903
Andreas <androlo1980@gmail.com>
2017-09-11 17:10:06 +0200

    try different container

--- .circleci/fetch_deps.sh
+++ .circleci/fetch_deps.sh
@@ -4,5 +4,7 @@ sudo apt-get -y install software-properties-common
 sudo add-apt-repository -y ppa:ethereum/ethereum
 sudo add-apt-repository -y ppa:ethereum/ethereum-dev
 sudo apt-get -y update
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
 sudo apt-get install -y cpp-ethereum
 sudo apt-get install -y solc
