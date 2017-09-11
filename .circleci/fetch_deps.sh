c27558562cd418330a99194c87d98691cd512f00
Andreas <androlo1980@gmail.com>
2017-09-11 20:46:18 +0200

    re add curl...

--- .circleci/fetch_deps.sh
+++ .circleci/fetch_deps.sh
@@ -3,7 +3,7 @@
 #sudo apt-get -y install software-properties-common
 #sudo add-apt-repository -y ppa:ethereum/ethereum
 #sudo add-apt-repository -y ppa:ethereum/ethereum-dev
-#sudo apt-get -y update
-#sudo apt-get install -y curl
+sudo apt-get -y update
+sudo apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs #testeth solc
